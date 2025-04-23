b6507ba2d08fc9533807d98fc375ce524c9d1ea9
Zach <zach@fdsnet.co.jp>
2025-04-23 12:07:27 +0900

    removed github pages

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -4,7 +4,10 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
 i18next.use(LanguageDetector).use(Backend).use(initReactI18next).init({
-    returnObjects: true,
-    fallbackLng: 'en',
-    debug: true,
-})
+  returnObjects: true,
+  fallbackLng: 'en',
+  debug: true,
+  backend: {
+    loadPath: '/locales/{{lng}}/{{ns}}.json', // Ensures correct loading path
+  },
+});
